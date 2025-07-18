60e9c598079ee5be1e220973d09d1c780efed461
JameNori <jame_nori@hotmail.com>
2025-07-18 15:30:25 +0700

    feat: implement light bulb status checks and student grade calculation

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,10 @@
 //Exercise 2
 let lightBulbStatus = "On";
+lightBulbStatus = "Broken"; // ถ้าใช้ค่าอื่น จะเข้าเงื่อนไข สุดท้าย “Please choose the correct input (On/Off/Broken)”
 
 // Start coding here.
+if (lightBulbStatus === "On") {
+  console.log("Light bulb is On.");
+} else if (lightBulbStatus === "Broken") {
+  console.log("Light bulb is Broken.");
+} else {console.log('Please'+' '+'choose'+' '+'the'+' '+'correct'+' '+'input'+' '+'(On/Off/Broken)');}
--- ex-6.js
+++ ex-6.js
@@ -2,7 +2,9 @@ let favoriteBooks = [];
 
 function addFavoriteBook(bookName) {
   // Start coding here !
-}
+  if(bookName.includes("Beauty")){return favoriteBooks.push(bookName);}
+
+} 
 
 // Example case
 addFavoriteBook("Let Me Tell You What I Mean");
@@ -12,3 +14,16 @@ addFavoriteBook("Black Beauty");
 addFavoriteBook("Monstrous Beauty");
 
 console.log(favoriteBooks);
+
+// let result = [];
+
+// function evenOrNot (numbers) {
+//     if (numbers.includes("2")) {
+//         result.push(numbers);
+//     }
+//     return result;
+// }
+
+
+// console.log(evenOrNot("11111"));
+// console.log(evenOrNot("12345"));
